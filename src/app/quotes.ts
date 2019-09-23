bdcb353d662d47bc86795823903b3379f71e3100
jay-68 <kelvinngari1995@gmail.com>
2019-09-23 04:55:54 +0300

    Add quotes array in the quote component.ts

---
+++ src/app/quotes.ts
@@ -0,0 +1,9 @@
+export class Quotes {
+
+  public showUploader: boolean;
+  // tslint:disable-next-line: max-line-length
+  constructor(public id: number, public name: string, public upvote: number, public downvote: number, public uploader: string, public completeDate: Date) {
+    this.showUploader = false;
+  }
+
+}
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -7,6 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
 
+  quotes = [
+    // tslint:disable-next-line: max-line-length
+    new Quotes(1, 'Live as if you were to die tomorrow. Learn as if you were to live forever by  Mahatma Gandhi', 0, 0, 'Brian', new Date(7 / 7 / 2019)),
+    new Quotes(2, 'That which does not kill us makes us stronger by  Friedrich Nietzsche', 0, 0, 'Brian', new Date(7 / 7 / 2019)),
+    // tslint:disable-next-line: max-line-length
+    new Quotes(3, 'We must not allow other people’s limited perceptions to define us by Virginia Satir', 0, 0, 'Brian', new Date(7 / 7 / 2019)),
+    new Quotes(4, 'If you cannot do great things, do small things in a great way by Napoleon Hill', 0, 0, 'Brian', new Date(7 / 7 / 2019)),
+  ];
+
+
+
+  togleDetails(index) {
+    this.quotes[index].showUploader = !this.quotes[index].showUploader;
+  }
+
+  deleteQuote(isComplete, index) {
+    if (isComplete) {
+      // tslint:disable-next-line: prefer-const
+      let toDelete = confirm(`Are you sure you want to delete quote ${this.quotes[index].id}`);
+      if (toDelete) {
+        this.quotes.splice(index, 1);
+      }
+    }
+  }
+
+  // tslint:disable-next-line: no-shadowed-variable
+  addNewQuote(Quotes: Quotes) {
+    // tslint:disable-next-line: prefer-const
+    let quoteLength = this.quotes.length;
+    Quotes.id = quoteLength + 1;
+    Quotes.completeDate = new Date(Quotes.completeDate);
+    this.quotes.push(Quotes);
+  }
+
   constructor() { }
 
   ngOnInit() {
